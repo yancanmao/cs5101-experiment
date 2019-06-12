efa52ea276cf4d0a1debfc61d850a6dbd9657fe8
maoyancan <maoyancan@gmail.com>
2019-06-12 20:40:33 +0800

    update

--- src/main/java/samzaapp/StockPrice.java
+++ src/main/java/samzaapp/StockPrice.java
@@ -1,6 +1,35 @@
 package samzaapp;
 
-public class StockPrice {
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableMap;
+import java.time.Duration;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import joptsimple.OptionSet;
+import org.apache.samza.application.StreamApplication;
+import org.apache.samza.application.descriptors.StreamApplicationDescriptor;
+import org.apache.samza.config.Config;
+import org.apache.samza.config.MapConfig;
+import org.apache.samza.operators.KV;
+import org.apache.samza.operators.MessageStream;
+import org.apache.samza.operators.OutputStream;
+import org.apache.samza.operators.functions.JoinFunction;
+import org.apache.samza.operators.windows.WindowPane;
+import org.apache.samza.operators.windows.Windows;
+import org.apache.samza.runtime.LocalApplicationRunner;
+import org.apache.samza.serializers.IntegerSerde;
+import org.apache.samza.serializers.KVSerde;
+import org.apache.samza.serializers.Serde;
+import org.apache.samza.serializers.StringSerde;
+import org.apache.samza.system.kafka.descriptors.KafkaInputDescriptor;
+import org.apache.samza.system.kafka.descriptors.KafkaOutputDescriptor;
+import org.apache.samza.system.kafka.descriptors.KafkaSystemDescriptor;
+import org.apache.samza.util.CommandLine;
+import org.apache.samza.util.Util;
+
+public class StockPrice implements StreamApplication  {
     private static final int Order_No = 0;
     private static final int Tran_Maint_Code = 1;
     private static final int Order_Price = 8;
@@ -8,4 +37,9 @@ public class StockPrice {
     private static final int Order_Vol = 10;
     private static final int Sec_Code = 11;
     private static final int Trade_Dir = 22;
+
+    @Override
+    public void describe(StreamApplicationDescriptor streamApplicationDescriptor) {
+
+    }
 }
